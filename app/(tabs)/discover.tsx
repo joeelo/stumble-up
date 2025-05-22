8a6fd5fa4228e4c06385165bc8a2346b9d690a37
joeelo <josephclorenzo@gmail.com>
2025-05-22 01:29:44 -0400

    proof of concept working

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -1,74 +1,89 @@
 import { ThemedText } from "@/components/ThemedText"
 import { ThemedView } from "@/components/ThemedView"
-import { EventSubscription, StyleSheet } from "react-native"
 import { useEffect, useState } from "react"
 import { IconSymbol } from "@/components/ui/IconSymbol"
 import ParallaxScrollView from "@/components/ParallaxScrollView"
-import { DeviceMotion, DeviceMotionMeasurement } from "expo-sensors"
+import { Accelerometer } from "expo-sensors"
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native"
+import WebView from "react-native-webview"
 
 export default function Discover() {
-  const [data, setData] = useState<Partial<DeviceMotionMeasurement>>({
-    acceleration: { x: 0, y: 0, z: 0, timestamp: 0 },
-    rotationRate: { alpha: 0, beta: 0, gamma: 0, timestamp: 0 }
+  const [{ x, y, z }, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0
   })
-  const [subscription, setSubscription] = useState<EventSubscription | null>(
-    null
-  )
+  // TODO: Make sure shakeactive is accepted by user
+  const [isShakeActive, setIsShakeActive] = useState(true) // State to manage shake activation
+  const [index, setIndex] = useState(0)
 
+  const randomWebsites = [
+    "https://reactnative.dev/",
+    "https://www.reddit.com",
+    "https://www.wikipedia.org",
+    "https://www.github.com",
+    "https://www.stackoverflow.com",
+    "https://www.medium.com",
+    "https://www.imgur.com",
+    "https://www.twitch.tv",
+    "https://www.spotify.com",
+    "https://www.discord.com",
+    "https://www.archive.org"
+  ]
+
+  console.log(randomWebsites[index])
+
+  // https://docs.expo.dev/versions/latest/sdk/accelerometer/
   useEffect(() => {
-    const isAvailable = async () => {
-      let available = await DeviceMotion.isAvailableAsync()
-      if (!available) {
-        alert("Device motion is not available on this device")
-      } else {
-        subscribe()
-      }
+    let subscription = undefined
+
+    if (isShakeActive) {
+      Accelerometer.setUpdateInterval(100) // Adjust based on your needs
+
+      subscription = Accelerometer.addListener((accelerometerData) => {
+        const { x } = accelerometerData
+        if (Math.abs(x) > 1.8) {
+          // Adjust threshold based on testing for x-axis motion
+          console.log("shake detected")
+          setIndex((prev) => prev + 1)
+        }
+      })
     }
 
-    isAvailable()
-
-    return () => unsubscribe()
-  }, [])
-
-  console.log(data)
-
-  const subscribe = () => {
-    DeviceMotion.setUpdateInterval(100) // Update every 100 milliseconds
-    const newSubscription = DeviceMotion.addListener((motionData) => {
-      setData(motionData)
-    })
-    // @ts-ignore
-    setSubscription(newSubscription)
-  }
-
-  const unsubscribe = () => {
-    subscription && subscription.remove()
-    setSubscription(null)
-  }
+    return () => {
+      subscription && subscription.remove()
+    }
+  }, [isShakeActive]) // Depend on isShakeActive to re-run effect
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-        />
-      }
-    >
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">Discover</ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    // https://blog.logrocket.com/react-native-webview-complete-guide/
+    <WebView source={{ uri: randomWebsites[index] }} style={styles.container} />
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 100,
+    marginTop: 30
+  },
+  text: {
+    textAlign: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    marginTop: 15
+  },
+  button: {
+    flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "#eee",
+    padding: 10
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "#ccc"
   }
 })
